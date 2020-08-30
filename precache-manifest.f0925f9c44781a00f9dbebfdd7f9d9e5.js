a724a2ae5a2d823a3c63569e6e3076ace538de84
Nermeen Mattar <v-nmattar@expedia.com>
2020-08-31 00:21:16 +0300

    Deploy to github pages

---
+++ precache-manifest.f0925f9c44781a00f9dbebfdd7f9d9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5026a6eab20a5180ffc9a0d00de8920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f78361a7c75722480ad",
+    "url": "/static/css/main.5a6e7903.chunk.css"
+  },
+  {
+    "revision": "cdb7dab666d031e1af91",
+    "url": "/static/js/2.eaf0c877.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eaf0c877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f78361a7c75722480ad",
+    "url": "/static/js/main.d40c9f4f.chunk.js"
+  },
+  {
+    "revision": "64cddc4e8240c9d458a2",
+    "url": "/static/js/runtime-main.7de513d3.js"
+  }
+]);
